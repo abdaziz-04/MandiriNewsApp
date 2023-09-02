199f5d50851ce20afe1a67c2a1e4dd9068a50ec2
abdaziz-04 <abdaziz1713@gmail.com>
2023-09-02 17:26:44 +0700

    Fixing bug

--- app/src/main/java/com/mandiri/newsapp/ui/MainActivity.java
+++ app/src/main/java/com/mandiri/newsapp/ui/MainActivity.java
@@ -1,19 +1,22 @@
 package com.mandiri.newsapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+
 import com.mandiri.newsapp.R;
 import com.mandiri.newsapp.model.News;
 import com.mandiri.newsapp.model.NewsResponse;
 import com.mandiri.newsapp.network.ApiUtils;
 import com.mandiri.newsapp.network.NewsApiService;
+
 import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -23,6 +26,8 @@ public class MainActivity extends AppCompatActivity implements NewsAdapter.OnIte
     private RecyclerView recyclerView;
     private ProgressBar progressBar;
     private NewsAdapter newsAdapter;
+    private int currentPage = 1;
+    private boolean isLoading = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,14 +41,40 @@ public class MainActivity extends AppCompatActivity implements NewsAdapter.OnIte
         newsAdapter = new NewsAdapter(this, this);
         recyclerView.setAdapter(newsAdapter);
 
+        // Set up infinite scrolling
+        setupScrollListener();
+
+        // Load initial news
         loadNews();
     }
 
+    private void setupScrollListener() {
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+                int visibleItemCount = layoutManager.getChildCount();
+                int totalItemCount = layoutManager.getItemCount();
+                int firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition();
+
+                if (!isLoading && (visibleItemCount + firstVisibleItemPosition) >= totalItemCount) {
+                    currentPage++;
+                    loadMoreNews();
+                    isLoading = true;
+                }
+            }
+        });
+    }
+
     private void loadNews() {
+        progressBar.setVisibility(View.VISIBLE);
+
         NewsApiService newsApiService = ApiUtils.getNewsApiService();
         Call<NewsResponse> call = newsApiService.getTopHeadlines(
                 NewsApiService.API_KEY,
-                1,
+                currentPage,
                 10
         );
 
@@ -53,7 +84,12 @@ public class MainActivity extends AppCompatActivity implements NewsAdapter.OnIte
                 if (response.isSuccessful()) {
                     NewsResponse newsResponse = response.body();
                     List<News> newsList = newsResponse.getArticles();
-                    newsAdapter.setData(newsList);
+                    if (currentPage == 1 && newsList.size() > 0) {
+                        // Set berita terbaru jika tersedia
+                        newsAdapter.setLatestNews(newsList.get(0));
+                        newsList.remove(0); // Hapus berita terbaru dari daftar
+                    }
+                    newsAdapter.addMoreData(newsList); // Tambahkan sisanya ke RecyclerView
                 } else {
                     Log.e("API Error", response.message());
                 }
@@ -68,10 +104,41 @@ public class MainActivity extends AppCompatActivity implements NewsAdapter.OnIte
         });
     }
 
+    private void loadMoreNews() {
+        progressBar.setVisibility(View.VISIBLE);
+
+        NewsApiService newsApiService = ApiUtils.getNewsApiService();
+        Call<NewsResponse> call = newsApiService.getTopHeadlines(
+                NewsApiService.API_KEY,
+                currentPage,
+                10
+        );
+
+        call.enqueue(new Callback<NewsResponse>() {
+            @Override
+            public void onResponse(Call<NewsResponse> call, Response<NewsResponse> response) {
+                if (response.isSuccessful()) {
+                    NewsResponse newsResponse = response.body();
+                    List<News> newsList = newsResponse.getArticles();
+                    newsAdapter.addMoreData(newsList);
+                } else {
+                    Log.e("API Error", response.message());
+                }
+                progressBar.setVisibility(View.GONE);
+                isLoading = false;
+            }
+
+            @Override
+            public void onFailure(Call<NewsResponse> call, Throwable t) {
+                Log.e("API Error", t.getMessage());
+                progressBar.setVisibility(View.GONE);
+                isLoading = false;
+            }
+        });
+    }
+
     @Override
     public void onItemClick(News news) {
-        Intent intent = new Intent(this, NewsDetailActivity.class);
-        intent.putExtra("news", news);
-        startActivity(intent);
+        // Tindakan yang akan diambil ketika item diklik
     }
 }
--- app/src/main/java/com/mandiri/newsapp/ui/NewsAdapter.java
+++ app/src/main/java/com/mandiri/newsapp/ui/NewsAdapter.java
@@ -5,18 +5,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.mandiri.newsapp.R;
 import com.mandiri.newsapp.model.News;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
+public class NewsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static final int TYPE_CARD = 1;
+    private static final int TYPE_LIST = 2;
 
     private List<News> newsList = new ArrayList<>();
     private Context context;
     private OnItemClickListener onItemClickListener;
+    private News latestNews;
 
     public interface OnItemClickListener {
         void onItemClick(News news);
@@ -27,42 +33,102 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
         this.onItemClickListener = onItemClickListener;
     }
 
-    public void setData(List<News> newsList) {
-        this.newsList = newsList;
+    // Set data for the latest news
+    public void setLatestNews(News news) {
+        latestNews = news;
         notifyDataSetChanged();
+    }
+
+    // Add more data to the RecyclerView
+    public void addMoreData(List<News> newData) {
+        int start = newsList.size();
+        newsList.addAll(newData);
+        notifyItemRangeInserted(start, newData.size());
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        // Determine the type of view based on the position
+        if (position == 0 && latestNews != null) {
+            return TYPE_CARD; // CardView for the latest news
+        } else {
+            return TYPE_LIST; // RecyclerView for other news
+        }
     }
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.item_news, parent, false);
-        return new ViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        if (viewType == TYPE_CARD) {
+            View cardView = inflater.inflate(R.layout.item_latest_news, parent, false);
+            return new CardViewHolder(cardView);
+        } else {
+            View listView = inflater.inflate(R.layout.item_news, parent, false);
+            return new ListViewHolder(listView);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        News news = newsList.get(position);
-        holder.titleTextView.setText(news.getTitle());
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (getItemViewType(position) == TYPE_CARD) {
+            // Bind data for the latest news (CardView)
+            CardViewHolder cardHolder = (CardViewHolder) holder;
+            cardHolder.bind(latestNews);
+        } else {
+            // Bind data for other news items (RecyclerView)
+            ListViewHolder listHolder = (ListViewHolder) holder;
+            listHolder.bind(newsList.get(position - 1)); // Subtract 1 to account for the latest news
+        }
     }
 
     @Override
     public int getItemCount() {
-        return newsList.size();
+        return latestNews != null ? newsList.size() + 1 : newsList.size();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder {
+    // ViewHolder for the latest news (CardView)
+    class CardViewHolder extends RecyclerView.ViewHolder {
         TextView titleTextView;
 
-        ViewHolder(@NonNull View itemView) {
+        CardViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleTextView = itemView.findViewById(R.id.latestNewsTitle);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onItemClickListener.onItemClick(latestNews);
+                }
+            });
+        }
+
+        void bind(News news) {
+            titleTextView.setText(news.getTitle());
+            // Bind other views for the latest news here
+        }
+    }
+
+    // ViewHolder for other news items (RecyclerView)
+    class ListViewHolder extends RecyclerView.ViewHolder {
+        TextView titleTextView;
+
+        ListViewHolder(@NonNull View itemView) {
             super(itemView);
             titleTextView = itemView.findViewById(R.id.titleTextView);
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    onItemClickListener.onItemClick(newsList.get(getAdapterPosition()));
+                    onItemClickListener.onItemClick(newsList.get(getAdapterPosition() - 1));
                 }
             });
+        }
+
+        void bind(News news) {
+            titleTextView.setText(news.getTitle());
+            // Bind other views for other news items here
         }
     }
 }
